e3f82e067fbd4af94dc59d62bd9b2ad9235a568e
Iris S <irisiseve@gmail.com>
2019-05-15 19:41:43 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,5 @@
 var playlist = { "Modest Mouse": "Night on the sun", "Porches": "Country", "Rae Sremmurd": "Yoga"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+}
